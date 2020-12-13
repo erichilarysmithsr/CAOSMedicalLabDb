01c17b1be10b50a26fdb9098b708dc9726808b08
Eric Hilary Smith <erichilarysmith@outlook.com>
2020-12-13 15:30:26 -0600

    Add files via upload

---
+++ caosmedicallab.py
@@ -0,0 +1,37 @@
+>>> S = 'Susceptible'
+>>> print(S)
+>>> E = 'Exposed'
+>>> print(E)
+>>> I = 'Infectious'
+>>> print(I)
+>>> R = 'Removed'
+>>> print(R)
+>>> N = 'Total Population'
+>>> print(N)
+>>> C = 'Living with COVID19'
+>>> print(C)
+>>> D = 'Living with Diabetes'
+>>> print(D)
+>>> Susceptible = input('Enter number of Susceptible Individuals')
+>>> print(Susceptible + 1)
+>>> print(int(Susceptible)+ 1)
+>>> Exposed = input('Enter number of Exposed Individuals')
+>>> print(Exposed + 1)
+>>> print(int(Exposed)+ 1)
+>>> Infectious = input('Enter number of Infectious Individuals')
+>>> print(Infectious + 1)
+>>> print(int(Infectious)+ 1)
+>>> Removed = input('Enter number of Removed Individuals')
+>>> print(Removed + 1)
+>>> print(int(Removed) + 1)
+>>> Total Population = input('Enter number of Total Population')
+>>> print(Total Population + 1)
+>>> print(int(Total Population)+ 1)
+>>> Living with COVID19 = input('Enter number of Individuals Living with COVID19')
+>>> print(Living with COVID19 + 1)
+>>> print(int(Living with COVID19)+ 1)
+>>> Living with Diabetes = input('Enter number of Individuals Living with Diabetes')
+>>> print(Living with Diabetes + 1)
+>>> print(int(Living with Diabetes)+ 1)
+>>> S = C + D - E - I - R
+>>> print(S)
