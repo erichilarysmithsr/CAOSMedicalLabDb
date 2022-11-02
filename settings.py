c3a06001d1c166948b1c97d951ac422b3a2738ec
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2022-11-02 08:33:49 -0500

    Create settings.py
    Created settings.py file for Bugsnag

---
+++ settings.py
@@ -0,0 +1,9 @@
+BUGSNAG = {
+    'api_key': '266593ad1c6a5201c9d8314c92c02358',
+    'project_root': '/path/to/your/app',
+}
+
+MIDDLEWARE = (
+    'bugsnag.django.middleware.BugsnagMiddleware',
+    # ...
+)
